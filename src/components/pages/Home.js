4a1a80a281f019e7169fd8d2ebe01e8f09bb8c66
hamidreza noruzinejad <hamidrezanoruzinejad@gmail.com>
2018-10-05 19:35:46 +0330

    6 cities with component developed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import { axios } from 'axios';
+// import { Link } from "react-router-dom";
+// import {Weather} from './Weather';
+import NoMatch from './NoMatch';
+import Weather from './Weather';
 
 class Home extends Component {
 
-    constructor(){
-        super();
-        this.setState({
-            cities : {},
-            status : 'loading',
-            server: 'http://localhost/'
-        });
-
-    }
 
     componentWillMount(){
+        this.setState({
+            cities:[],
+            loading : true
+        })
+        var that = this;
+        let cities_woeid = {Istanbul:2344116, Berlin:638242, London:44418, Helsinki:565346, Dublin:560743, Vancouver:9807};
+        let cities = [];
+        for (let city in cities_woeid){
+            fetch(`http://localhost/weather.php?command=location&woeid=${cities_woeid[city]}`)
+            .then(result => result.json().then(res=>{
+                cities.push(res);
+                that.setState({ cities });
+            }))    
+        }
 
+        this.setState({
+            loading: false
+        })
+        
     }
+
+    renderCities(){
+        if(this.state.loading!= null && !this.state.loading ){
+            return this.state.cities.map(function(index){
+                console.log(index);
+                return <Weather city={index} cityWeather={index.consolidated_weather[0]} day="today" key={index.consolidated_weather[0].id}/>;
+            })
+        }
+        return false;
+    }
+
 
 
     render() {
         return (
 
-            <div class="site-content">
+            <div className="site-content">
 
 
-                <div class="hero" data-bg-image="images/banner.png">
-                    <div class="container">
-                        <form action="#" class="find-location">
+                <div className="hero" data-bg-image="images/banner.png">
+                    <div className="container">
+                        <form action="#" className="find-location">
                             <input type="text" placeholder="Find your location..." />
                             <input type="submit" value="Find" />
                         </form>
@@ -35,73 +57,28 @@ class Home extends Component {
                     </div>
                 </div>
 
-                <div class="forecast-table">
-                    <div class="container">
-                        <div class="forecast-container">
-                            <div class="today forecast">
-                                <div class="forecast-header">
-                                    <div class="day">Monday</div>
-                                    <div class="date">6 Oct</div>
-                                </div>
-                                <div class="forecast-content">
-                                    <div class="location">New York</div>
-                                    <div class="degree">
-                                        <div class="num">23<sup>o</sup>C</div>
-                                        <div class="forecast-icon">
-                                            <img src="https://www.metaweather.com/static/img/weather/sn.svg" alt="" width="98" />
-                                        </div>
-                                    </div>
-                                    <span><img src="images/icon-umberella.png" alt="" />20%</span>
-                                    <span><img src="images/icon-wind.png" alt="" />18km/h</span>
-                                    <span><img src="images/icon-compass.png" alt="" />East</span>
-                                </div>
-                            </div>
-                            <div class="today forecast">
-                                <div class="forecast-header">
-                                    <div class="day">Monday</div>
-                                    <div class="date">6 Oct</div>
-                                </div>
-                                <div class="forecast-content">
-                                    <div class="location">New York</div>
-                                    <div class="degree">
-                                        <div class="num">23<sup>o</sup>C</div>
-                                        <div class="forecast-icon">
-                                            <img src="images/icons/icon-1.svg" alt="" />
-                                        </div>
-                                    </div>
-                                    <span><img src="images/icon-umberella.png" alt="" />20%</span>
-                                    <span><img src="images/icon-wind.png" alt="" />18km/h</span>
-                                    <span><img src="images/icon-compass.png" alt="" />East</span>
-                                </div>
-                            </div>
+                {this.renderCities()}
 
-                        </div>
-                    </div>
-                </div>
-
-                <footer class="site-footer" >
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8">
-                                <form action="#" class="subscribe-form">
+                <footer className="site-footer" >
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8">
+                                <form action="#" className="subscribe-form">
                                     <input type="text" placeholder="Enter your email to subscribe..." />
                                     <input type="submit" value="Subscribe" />
                                 </form>
                             </div>
-                            <div class="col-md-3 col-md-offset-1">
-                                <div class="social-links">
+                            <div className="col-md-3 col-md-offset-1">
+                                <div className="social-links">
 
                                 </div>
                             </div>
                         </div>
 
-                        <p class="colophon">Copyright 2018, All rights reserved</p>
+                        <p className="colophon">Copyright 2018, All rights reserved</p>
                     </div>
                 </footer>
             </div>
-
-
-
         );
     }
 }
--- src/components/pages/Weather.js
+++ src/components/pages/Weather.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 class Weather extends Component {
     render() {
-        const { params } = this.props.match;
-        console.log(params);
+        const { city, cityWeather , day } = this.props;
+        let image=`https://www.metaweather.com/static/img/weather/${cityWeather.weather_state_abbr}.svg`;
         return (
-            <div>
-                <h2>Weather : {params.woeid}</h2>
+            <div className="forecast-table">
+                <div className="container">
+                    <div className="forecast-container">
+                        <div className="today forecast">
+                            <div className="forecast-header">
+                                <div className="day">{day}</div>
+                                <div className="date">{cityWeather.applicable_date}</div>
+                            </div>
+                            <div className="forecast-content">
+                                <div className="location">{city.title}</div>
+                                <div className="degree">
+                                    <div className="num">{Math.round(cityWeather.min_temp)}<sup>o</sup>C - </div>
+                                    <div className="num">{Math.round(cityWeather.max_temp)}<sup>o</sup>C</div>
+                                    <div className="forecast-icon">
+                                        <img src={image} alt="" width="98" />
+                                    </div>
+                                </div>
+                                <span><img src="images/icon-umberella.png" alt="" />{cityWeather.humidity}%</span>
+                                <span><img src="images/icon-wind.png" alt="" />{Math.round(cityWeather.wind_speed)}km/h</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
